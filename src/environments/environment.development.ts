9622a0b00121d0942cccfe5c52efff29565fcaca
RagulSTech <ragulmani097@gmail.com>
2026-05-02 22:53:30 +0530

    Initial commit with live API configuration

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://ewoms-application.tryasp.net/api'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'http://ewoms-application.tryasp.net/api',
+  chatServerUrl: 'http://ewoms-application.tryasp.net' // Adjust this if you use a separate chat server
+};
